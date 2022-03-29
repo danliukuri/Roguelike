449b98859fd88e4316ebf7582b6e15e789ac3bb1
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2022-03-29 21:26:27 +0300

    Bind IRoomsPlacer to RoomsPlacer on scene opening

---
+++ Assets/Scripts/Installers/Scene/PlacersInstaller.cs
@@ -0,0 +1,24 @@
+﻿using Roguelike.Core.Placers;
+using Roguelike.Placers;
+using Zenject;
+
+namespace Roguelike.Installers.Scene
+{
+    public class PlacersInstaller : MonoInstaller
+    {
+        #region Methods
+        public override void InstallBindings()
+        {
+            BindRoomsPlacer();
+        }
+
+        void BindRoomsPlacer()
+        {
+            Container
+                .Bind<IRoomsPlacer>()
+                .To<RoomsPlacer>()
+                .AsSingle();
+        }
+        #endregion
+    }
+}
