59edd543002567d86d512198fc1623d11e41e44c
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2022-04-03 20:12:36 +0300

    Create WeightAndValue.cs Used for sequences where weights are needed.

---
+++ Assets/Scripts/Utilities/WeightAndValue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Roguelike.Utilities
+{
+    [Serializable]
+    public class WeightAndValue<T>
+    {
+        #region Properties
+        public T Value { get => value; set => this.value = value; }
+        public int Weight { get => weight; set => weight = value < MinWeight ? MinWeight : value; }
+        #endregion
+
+        #region Fields
+        [SerializeField] T value;
+        [SerializeField, Min(MinWeight)] int weight;
+
+        const int MinWeight = default;
+        #endregion
+    }
+}
