930810b821c96c55413be0c2d7edc3ae1c5f3ae9
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2022-04-03 21:21:27 +0300

    Create TransformExtensions.cs Create a method to rotate a transform a random number of times by a right angle.

---
+++ Assets/Scripts/Utilities/Extensions/TransformExtensions.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Roguelike.Utilities.Extensions
+{
+    public static class TransformExtensions
+    {
+        #region Methods
+        public static void RotateRandomNumberOfTimesByRightAngle(this Transform sourse,
+            Vector3 axis, System.Action onRotateAction = null)
+        {
+            sourse.CheckForNullException(nameof(sourse));
+            axis.CheckForNullException(nameof(axis));
+
+            const float RightAngle = 90f;
+            const int MinNumberOfTurns = 0, MaxNumberOfTurns = 3;
+
+            int randomNumberOfTurns = Random.Range(MinNumberOfTurns, MaxNumberOfTurns + 1);
+            for (int i = 0; i < randomNumberOfTurns; i++)
+            {
+                sourse.Rotate(axis, RightAngle);
+                onRotateAction?.Invoke();
+            }
+        }
+        #endregion
+    }
+}
