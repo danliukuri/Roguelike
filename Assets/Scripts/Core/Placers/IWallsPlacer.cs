ab86f4d0eb64f7802676ae98ef13409b1d338ec8
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2022-04-04 15:25:23 +0300

    Create WallsPlacer

---
+++ Assets/Scripts/Core/Placers/IWallsPlacer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Roguelike.Core.Placers
+{
+    public interface IWallsPlacer
+    {
+        void Place(List<Transform> wallsMarkers);
+    }
+}
---
+++ Assets/Scripts/Placers/WallsPlacer.cs
@@ -0,0 +1,32 @@
+﻿using Roguelike.Core.Factories;
+using Roguelike.Core.Placers;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Roguelike.Placers
+{
+    public class WallsPlacer : IWallsPlacer
+    {
+        #region Fields
+        readonly IWallFactory wallFactory;
+        #endregion
+
+        #region Methods
+        public WallsPlacer(IWallFactory wallFactory) => this.wallFactory = wallFactory;
+        public void Place(List<Transform> wallsMarkers)
+        {
+            foreach (Transform wallMarker in wallsMarkers)
+                if(wallMarker.gameObject.activeSelf)
+                {
+                    GameObject wallGameObject = wallFactory.GetWall();
+                    Transform wallTransform = wallGameObject.transform;
+                    
+                    wallTransform.position = wallMarker.position;
+                    wallTransform.SetParent(wallMarker);
+
+                    wallGameObject.SetActive(true);
+                }
+        }
+        #endregion
+    }
+}
