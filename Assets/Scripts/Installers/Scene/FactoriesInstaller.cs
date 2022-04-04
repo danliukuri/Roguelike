3277d537914df805015cce9f7e0baf829096435d
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2022-04-04 15:28:42 +0300

    Bind IWallFactory to wall factory on scene opening

--- Assets/Scripts/Installers/Scene/FactoriesInstaller.cs
+++ Assets/Scripts/Installers/Scene/FactoriesInstaller.cs
@@ -7,16 +7,27 @@ namespace Roguelike.Installers.Scene
     public class FactoriesInstaller : MonoInstaller
     {
         #region Fields 
+        [SerializeField] GameObject wallFactory;
         [SerializeField] GameObject[] roomsFactories;
         #endregion
 
         #region Methods
         public override void InstallBindings()
         {
+            BindWallFactory();
             foreach (GameObject roomFactory in roomsFactories)
                 BindRoomFactory(roomFactory);
         }
 
+        void BindWallFactory()
+        {
+            Container
+                .Bind<IWallFactory>()
+                .FromComponentInNewPrefab(wallFactory)
+                .UnderTransform(transform)
+                .AsSingle()
+                .NonLazy();
+        }
         void BindRoomFactory(GameObject roomFactory)
         {
             Container
