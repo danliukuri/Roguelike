c4249d0200c68b8066dc61439cf37c34b2763e92
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2022-04-08 15:39:26 +0300

    Create door and exit placers

---
+++ Assets/Scripts/Placers/DoorsPlacer.cs
@@ -0,0 +1,32 @@
+﻿using Roguelike.Core.Factories;
+using Roguelike.Core.Placers;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Roguelike.Placers
+{
+    public class DoorsPlacer : IDoorsPlacer
+    {
+        #region Fields
+        readonly IDoorFactory doorFactory;
+        #endregion
+
+        #region Methods
+        public DoorsPlacer(IDoorFactory doorFactory) => this.doorFactory = doorFactory;
+        public void Place(List<Transform> doorsMarkers)
+        {
+            foreach (Transform doorMarker in doorsMarkers)
+                if (doorMarker.gameObject.activeSelf)
+                {
+                    GameObject doorGameObject = doorFactory.GetDoor();
+                    Transform doorTransform = doorGameObject.transform;
+
+                    doorTransform.position = doorMarker.position;
+                    doorTransform.SetParent(doorMarker);
+
+                    doorGameObject.SetActive(true);
+                }
+        }
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Placers/ExitsPlacer.cs
@@ -0,0 +1,32 @@
+﻿using Roguelike.Core.Factories;
+using Roguelike.Core.Placers;
+using Roguelike.Utilities.Extensions;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Roguelike.Placers
+{
+    public class ExitsPlacer : IExitsPlacer
+    {
+        #region Fields
+        readonly IExitFactory exitFactory;
+        #endregion
+
+        #region Methods
+        public ExitsPlacer(IExitFactory exitFactory) => this.exitFactory = exitFactory;
+        public void Place(List<Transform> exitsMarkers)
+        {
+            Transform exitMarker = exitsMarkers.Where(m => m.gameObject.activeSelf).Random();
+
+            GameObject exitGameObject = exitFactory.GetExit();
+            Transform exitTransform = exitGameObject.transform;
+
+            exitTransform.position = exitMarker.position;
+            exitTransform.SetParent(exitMarker);
+
+            exitGameObject.SetActive(true);
+        }
+        #endregion
+    }
+}
