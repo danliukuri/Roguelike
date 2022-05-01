9ed880e794cd45ae39ea263e0ca72eac78bb0334
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2022-05-01 16:07:03 +0300

    Create Player

---
+++ Assets/Scripts/Movers/PlayerMover.cs
@@ -0,0 +1,37 @@
+﻿using Roguelike.Core.Services.Input;
+using UnityEngine;
+using Zenject;
+
+namespace Roguelike.Movers
+{
+    public class PlayerMover : MonoBehaviour
+    {
+        #region Fields
+        [SerializeField] float movementStep = 1f;
+
+        IMovementInputService movementInputService;
+        #endregion
+
+        #region Methods
+        [Inject]
+        public void Construct(IMovementInputService movementInputService)
+        {
+            this.movementInputService = movementInputService;
+        }
+
+        void OnEnable()
+        {
+            movementInputService.Moving += Move;
+        }
+        void OnDisable()
+        {
+            movementInputService.Moving -= Move;
+        }
+
+        void Move(Vector3 translation)
+        {
+            transform.Translate(translation * movementStep);
+        }
+        #endregion
+    }
+}
