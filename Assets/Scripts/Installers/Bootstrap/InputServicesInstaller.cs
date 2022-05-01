46db27c6cea0a1322cb10e461252972d474ad0ed
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2022-05-01 16:17:01 +0300

    Bind interfaces to StandaloneUserMovementInput on bootstrap

---
+++ Assets/Scripts/Installers/Bootstrap/InputServicesInstaller.cs
@@ -0,0 +1,29 @@
+﻿using Roguelike.Services.Input;
+using UnityEngine;
+using Zenject;
+
+namespace Roguelike.Installers.Bootstrap
+{
+    public class InputServicesInstaller : MonoInstaller
+    {
+        #region Fields
+        [SerializeField] float timeToHoldMoveKeyBeforeMoving;
+        #endregion
+
+        #region Methods
+        public override void InstallBindings()
+        {
+            BindMovementInputService();
+        }
+
+        void BindMovementInputService()
+        {
+            Container
+                .BindInterfacesTo<StandaloneUserMovementInput>()
+                .FromNew()
+                .AsSingle()
+                .WithArguments(timeToHoldMoveKeyBeforeMoving);
+        }
+        #endregion
+    }
+}
