5091198782d12e7238319dd0a1ef2058c4b6c513
Yuriy Danyliuk <yuradaniliyk@gmail.com>
2022-07-07 14:34:46 +0300

    Refactor extensions and fix grammar mistakes

--- Assets/Scripts/Utilities/Extensions/CollectionExtensions.cs
+++ Assets/Scripts/Utilities/Extensions/CollectionExtensions.cs
@@ -10,26 +10,26 @@ namespace Roguelike.Utilities.Extensions
         /// Return a random item from the sequence.
         /// </summary>
         /// <typeparam name="T">Type of sequence's items.</typeparam>
-        /// <param name="sourse">Sequence of items.</param>
+        /// <param name="source">Sequence of items.</param>
         /// <returns>A random item from the sequence.</returns>
-        public static T Random<T>(this ICollection<T> sourse) => sourse
-            .CheckForNullException(nameof(sourse))
-            .CheckForNoElementsException(sourse.Count)
-            .ElementAt(sourse.RandomIndex(true));
+        public static T Random<T>(this ICollection<T> source) => source
+            .CheckForNullException(nameof(source))
+            .CheckForNoElementsException(source.Count)
+            .ElementAt(source.RandomIndex(true));
 
         /// <summary>
-        /// Return an index of random item from the sequence.
+        /// Return an index of a random item from the sequence.
         /// </summary>
         /// <typeparam name="T">Type of sequence's items.</typeparam>
-        /// <param name="sourse">Sequence of items.</param>
-        /// <returns>An index of random item from the sequence.</returns>
-        public static int RandomIndex<T>(this ICollection<T> sourse) => sourse.RandomIndex(false);
-        static int RandomIndex<T>(this ICollection<T> sourse, bool isSourseChecked = false)
+        /// <param name="source">Sequence of items.</param>
+        /// <param name="isSourceChecked">If the source has already been checked for possible exceptions.</param>
+        /// <returns>An index of a random item from the sequence.</returns>
+        public static int RandomIndex<T>(this ICollection<T> source, bool isSourceChecked = false)
         {
-            if (!isSourseChecked)
-                sourse.CheckForNullException(nameof(sourse)).CheckForNoElementsException(sourse.Count);
+            if (!isSourceChecked)
+                source.CheckForNullException(nameof(source)).CheckForNoElementsException(source.Count);
 
-            return UnityEngine.Random.Range(0, sourse.Count);
+            return UnityEngine.Random.Range(0, source.Count);
         }
 
         /// <summary>
@@ -39,17 +39,17 @@ namespace Roguelike.Utilities.Extensions
         /// The greater the weight of the item among others, the more likely it is to be selected.
         /// </remarks>
         /// <typeparam name="T">Type of sequence's items values.</typeparam>
-        /// <param name="sourse">Sequence of <see cref="WeightAndValue{T}"/> items.</param>
+        /// <param name="source">Sequence of <see cref="WeightAndValue{T}"/> items.</param>
         /// <returns>A random item based on weights from the sequence.</returns>
-        public static WeightAndValue<T> RandomBasedOnWeights<T>(this ICollection<WeightAndValue<T>> sourse)
+        public static WeightAndValue<T> RandomBasedOnWeights<T>(this ICollection<WeightAndValue<T>> source)
         {
-            sourse.CheckForNullException(nameof(sourse)).CheckForNoElementsException(sourse.Count);
+            source.CheckForNullException(nameof(source)).CheckForNoElementsException(source.Count);
 
             const int MinWeight = 0;
-            int maxWeight = sourse.Sum(item => item.Weight);
+            int maxWeight = source.Sum(item => item.Weight);
             int currentWeight = UnityEngine.Random.Range(MinWeight, maxWeight);
 
-            return sourse.First(item => (currentWeight -= item.Weight) < MinWeight);
+            return source.First(item => (currentWeight -= item.Weight) < MinWeight);
         }
         #endregion
     }
--- Assets/Scripts/Utilities/Extensions/TransformExtensions.cs
+++ Assets/Scripts/Utilities/Extensions/TransformExtensions.cs
@@ -1,25 +1,35 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Roguelike.Utilities.Extensions
 {
     public static class TransformExtensions
     {
         #region Methods
-        public static void RotateRandomNumberOfTimesByRightAngle(this Transform sourse,
-            Vector3 axis, System.Action onRotateAction = null)
+        public static void RotateRandomNumberOfTimesByRightAngle(this Transform source, Vector3 axis,
+            Action onRotateAction = default)
         {
-            sourse.CheckForNullException(nameof(sourse));
+            source.CheckForNullException(nameof(source));
             axis.CheckForNullException(nameof(axis));
-
-            const float RightAngle = 90f;
+            
             const int MinNumberOfTurns = 0, MaxNumberOfTurns = 3;
-
             int randomNumberOfTurns = Random.Range(MinNumberOfTurns, MaxNumberOfTurns + 1);
             for (int i = 0; i < randomNumberOfTurns; i++)
+                source.RotateRightAngle(axis, onRotateAction, true);
+        }
+        public static void RotateRightAngle(this Transform source, Vector3 axis, Action onRotateAction = default,
+            bool isParametersChecked = false)
+        {
+            if (!isParametersChecked)
             {
-                sourse.Rotate(axis, RightAngle);
-                onRotateAction?.Invoke();
+                source.CheckForNullException(nameof(source));
+                axis.CheckForNullException(nameof(axis)); 
             }
+            
+            const float RightAngle = 90f;
+            source.Rotate(axis, RightAngle);
+            onRotateAction?.Invoke();
         }
         #endregion
     }
